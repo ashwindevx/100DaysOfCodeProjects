29e73230f5bc1911b25d0cd604fbd66b0dc80549
ashwindevx <ashwinchauhan.developer@gmail.com>
2021-08-08 20:52:30 +0530

    added components

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Layout/Header.js";
+import Meals from "./components/Meals/Meals.js";
 
 function App() {
   return (
     <div>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </div>
   );
 }
