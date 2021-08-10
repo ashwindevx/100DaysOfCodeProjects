f7c8bf07d51e07df23de1078079bb28d02d42993
ashwindevx <ashwinchauhan.developer@gmail.com>
2021-08-10 21:18:22 +0530

    added main components

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Header from "./components/Layout/Header.js";
 import Meals from "./components/Meals/Meals.js";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
-    <div>
+    <>
+      <Cart />
       <Header />
       <main>
         <Meals />
       </main>
-    </div>
+    </>
   );
 }
 
---
+++ food-app/src/components/UI/Modal.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return ReactDOM.createPortal(
+    <>
+      <Backdrop />
+      <ModalOverlay>{props.children}</ModalOverlay>
+    </>,
+    document.getElementById("overlays")
+  );
+};
+
+export default Modal;
